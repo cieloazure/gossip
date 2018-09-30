e5fb1c893b835a1ab30961f223a925366088c913
Akash S <akash.shingte@gmail.com>
2018-09-30 19:09:03 -0400

    Added a progress bar

--- lib/gossip/monitor.ex
+++ lib/gossip/monitor.ex
@@ -6,8 +6,10 @@ defmodule Gossip.Monitor do
   end
 
   def init(opts) do
+    IO.puts "Execution status"
     num_nodes = Keyword.get(opts, :num_nodes)
-    {:ok, child_pids} = Gossip.P2PSupervisor.start_children(Gossip.P2PSupervisor, num_nodes, self(), "line")
+    topology = Keyword.get(opts, :topology)
+    {:ok, child_pids} = Gossip.P2PSupervisor.start_children(Gossip.P2PSupervisor, num_nodes, self(), topology)
     pid = Enum.random(child_pids)
     send(pid, {:fact, 42, -1, nil})
     convergence_events = []
@@ -15,14 +17,14 @@ defmodule Gossip.Monitor do
   end
 
   def handle_info({:convergence_event, pid}, {convergence_events, num_nodes}) do
+
     #IO.puts "Received convergence event from #{inspect(pid)}"
     convergence_events = [pid | convergence_events]
     len_convergence_events = length(convergence_events)
     if len_convergence_events == num_nodes - 1 do
-      IO.puts "Convergence for network reached! Shutting monitor down!"
       System.stop(1)
     else
-      IO.puts "Execution #{Float.round((len_convergence_events/(num_nodes - 1) )* 100, 2)} % complete....."
+      ProgressBar.render(len_convergence_events, num_nodes)
     end
     {:noreply, {convergence_events, num_nodes}}
   end
--- proj2.exs
+++ proj2.exs
@@ -1,9 +1,10 @@
 usage_string = "Invalid arguments to the command\nUsage: mix run proj2.exs --no-halt <numNodes:int> <topology:string> <algorithm:string>"
-if length(System.argv) != 1 do
+if length(System.argv) != 2 do
    raise(ArgumentError, usage_string)
 end
-numNodes = System.argv
-Gossip.Monitor.start_link([num_nodes: numNodes])
+#TODO : Add error checking
+[num_nodes, topology]  = System.argv
+Gossip.Monitor.start_link([num_nodes: String.to_integer(num_nodes), topology: topology])
 #Gossip.P2PSupervisor.start_children(Gossip.P2PSupervisor, String.to_integer(numNodes), topology, algorithm)
 # {t, {:ok, child_pids}} = :timer.tc(Gossip.P2PSupervisor, :start_children, [Gossip.P2PSupervisor, String.to_integer(numNodes), topology, algorithm])
 #
