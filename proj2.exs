9c44919a4e1c1a65b8594892493987bbc3658226
tripp399 <p.tripathi@ufl.edu>
2018-09-29 21:18:23 -0400

    updated proj2.exs

--- proj2.exs
+++ proj2.exs
@@ -1,12 +1,7 @@
 usage_string = "Invalid arguments to the command\nUsage: mix run proj2.exs --no-halt <numNodes:int> <topology:string> <algorithm:string>"
-try do
-  if length(System.argv) != 3 do
-    IO.puts "--------- #{length(System.argv)}"
-    raise ArgumentError
-  end
-  [numNodes, topology, algorithm] = Enum.map(System.argv, fn x -> x end)
-  Gossip.P2PSupervisor.start_children(Gossip.P2PSupervisor, String.to_integer(numNodes), topology, algorithm)
-  # {t, {:ok, child_pids}} = :timer.tc(Gossip.P2PSupervisor, :start_children, [Gossip.P2PSupervisor, String.to_integer(numNodes), topology, algorithm])
-rescue
-  _e in ArgumentError -> IO.puts usage_string
-end
+if length(System.argv) != 3 do
+   raise(ArgumentError, usage_string)
+ end
+[numNodes, topology, algorithm] = Enum.map(System.argv, fn x -> x end)
+Gossip.P2PSupervisor.start_children(Gossip.P2PSupervisor, String.to_integer(numNodes), topology, algorithm)
+# {t, {:ok, child_pids}} = :timer.tc(Gossip.P2PSupervisor, :start_children, [Gossip.P2PSupervisor, String.to_integer(numNodes), topology, algorithm])
