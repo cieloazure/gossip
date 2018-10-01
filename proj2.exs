368e6f4fda0e6592e00312db7d35e92338c815da
Akash S <akash.shingte@gmail.com>
2018-09-30 23:37:48 -0400

    Change in proj2 script

--- proj2.exs
+++ proj2.exs
@@ -1,8 +1,8 @@
 usage_string = "Invalid arguments to the command\nUsage: mix run proj2.exs --no-halt <numNodes:int> <topology:string> <algorithm:string>"
-if length(System.argv) != 2 do
+if length(System.argv) != 3 do
    raise(ArgumentError, usage_string)
 end
 #TODO : Add error checking
-[num_nodes, topology]  = System.argv
-{:ok, pid} = Gossip.ConvergenceMonitor.start_link([num_nodes: String.to_integer(num_nodes), topology: topology, algorithm: "gossip"])
+[num_nodes, topology, algorithm]  = System.argv
+{:ok, pid} = Gossip.ConvergenceMonitor.start_link([num_nodes: String.to_integer(num_nodes), topology: topology, algorithm: algorithm])
 Gossip.ConvergenceMonitor.start_simulation(pid)
