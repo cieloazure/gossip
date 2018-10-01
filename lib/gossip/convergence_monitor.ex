d304ea037810d6b39f4fbbaacb57398d4205c5c3
Akash S <akash.shingte@gmail.com>
2018-10-01 00:55:03 -0400

    Verify state for gossip

--- lib/gossip/convergence_monitor.ex
+++ lib/gossip/convergence_monitor.ex
@@ -15,18 +15,26 @@ defmodule Gossip.ConvergenceMonitor do
     topology = Keyword.get(opts, :topology)
     algorithm = Keyword.get(opts, :algorithm)
     convergence_events = []
-    {:ok, {convergence_events, num_nodes, topology, algorithm}}
+    {:ok, {convergence_events, num_nodes, topology, algorithm, []}}
   end
 
-  def handle_call({:start}, _from, {_convergence_events, num_nodes, topology, algorithm} = state) do
+  def handle_call(
+        {:start},
+        _from,
+        {convergence_events, num_nodes, topology, algorithm, old_child_pids}
+      ) do
     {:ok, child_pids} =
       Gossip.Supervisor.start_children(Gossip.Supervisor, num_nodes, self(), topology)
 
     Gossip.Supervisor.initiate_algorithm(child_pids, algorithm)
-    {:reply, :ok, state}
+    new_child_pids = old_child_pids ++ child_pids
+    {:reply, :ok, {convergence_events, num_nodes, topology, algorithm, new_child_pids}}
   end
 
-  def handle_info({:convergence_event, pid}, {convergence_events, num_nodes, topology, algorithm}) do
+  def handle_info(
+        {:convergence_event, pid},
+        {convergence_events, num_nodes, topology, algorithm, child_pids}
+      ) do
     try do
       Logger.info("Convergence of node #{inspect(pid)}")
     rescue
@@ -38,11 +46,21 @@ defmodule Gossip.ConvergenceMonitor do
 
     if len_convergence_events >= num_nodes do
       ProgressBar.render(num_nodes, num_nodes)
-      System.stop(0)
+
+      IO.inspect(
+        Enum.map(child_pids, fn pid ->
+          {_, fact, count, status, _, _} = :sys.get_state(pid)
+          {pid, fact, count, status}
+        end),
+        limit: :infinity
+      )
+
+      # Process.sleep(1000)
+      # System.stop(0)
     else
       ProgressBar.render(len_convergence_events, num_nodes)
     end
 
-    {:noreply, {convergence_events, num_nodes, topology, algorithm}}
+    {:noreply, {convergence_events, num_nodes, topology, algorithm, child_pids}}
   end
 end
